759ada35fe477ce2cc6cab4b4ae057158e7ccd5b
gdmarketing <george@gdmarketing.pro>
2026-02-25 22:54:49 +0200

    Create page.tsx

---
+++ app/admin/page.tsx
@@ -0,0 +1,12 @@
+export default function Admin() {
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <title>GD Marketing Editor</title>
+        <script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"></script>
+      </head>
+      <body />
+    </html>
+  );
+}
